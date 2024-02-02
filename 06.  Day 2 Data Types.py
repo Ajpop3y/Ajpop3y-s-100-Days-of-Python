c5067b639766a3736c1edaaca43ca6d4a718b6a5
Ajpop3y <155366498+Ajpop3y@users.noreply.github.com>
2024-02-02 22:07:43 +0100

    Create 06.  Day 2 Data Types.py

---
+++ 06.  Day 2 Data Types.py
@@ -0,0 +1,8 @@
+Write a program that adds the digits in a 2 digit number. e.g. if the input was 35, then the output should be 3 + 5 = 8
+
+two_digit_number = input()
+first_digit = int(two_digit_number [0])
+second_digit = int(two_digit_number [1])
+print(first_digit + second_digit)
+# note [] gets the character in the index position stated by the number in the square brackets for any string.
+# In a list,it gets the item at the index position stated.
