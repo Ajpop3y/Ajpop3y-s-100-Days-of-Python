103bd463a088d647183b160db199addc805c0484
Ajpop3y <155366498+Ajpop3y@users.noreply.github.com>
2024-02-02 22:04:09 +0100

    Update 05. Variables switcherooroo.py

--- 05. Variables switcherooroo.py
+++ 05. Variables switcherooroo.py
@@ -5,13 +5,11 @@ Write a program that switches the values stored in the variables a and b.
 # There are two variables, a and b from input
 a = input("29")
 b = input("42")
-# 🚨 Don't change the code above ☝️
-####################################
-# Write your code below this line 👇
+
 c = a
 a = b
 b = c
-# 🚨 Don't change the code below 👇
+
 print("a: " + a)
 print("b: " + b)
 #"a:" is just a string. c assumes the value of original value of a, a assumes a new value : b, b assumes a new value c
