275031e5ff07b567adc11ed88ed83b4fdeb3f0c2
Ajpop3y <155366498+Ajpop3y@users.noreply.github.com>
2024-02-02 22:12:41 +0100

    Create 07. Data Types more (f strings, data conversion).py

---
+++ 07. Data Types more (f strings, data conversion).py
@@ -0,0 +1,29 @@
+#Data types
+
+#Strings
+#print("Ogunmepon"[8])
+# The square brackets are used to subscript, that is; to obtain a character from a set of characters - a string.
+
+#Integer
+print(123 + 234) #write plainly without quotes
+print(123_234) #_=,
+#Programming lingo for whole numbers
+
+#Float
+#numbers with decimal points
+
+#Boolean: True or False
+#Answers true of false question,begins with capital letter
+
+#f-Strings
+# score = 0
+# height = 1.8
+# isWinning = True
+# print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+
+
+
+# num_1 = len(input("What is your name?\n"))
+# new_num_1 = str(num_1) #conversion of one data type to another,
+# print("Hello, there are " + new_num_1 + " characters in your name")
+# print(type(new_num_1))
