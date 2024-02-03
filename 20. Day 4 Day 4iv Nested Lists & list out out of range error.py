0385db68c5400e9f540e3f9245d8b9f95f724db5
Ajpop3y <155366498+Ajpop3y@users.noreply.github.com>
2024-02-03 01:09:53 +0100

    Create 20. Day 4 Day 4iv Nested Lists & list out out of range error.py

---
+++ 20. Day 4 Day 4iv Nested Lists & list out out of range error.py
@@ -0,0 +1,8 @@
+12 = ["Eminem", "Bizzare", "Swifty Mcfae", "Proof" ]
+big_time_rush = ["Kendall", "James", "Logan", "Carlos"]
+BTS = ["RM", "V", "Suga", "Jimin"]
+
+boy_band = [d12, big_time_rush, BTS]
+print(boy_band[2][2])
+# The first [2] is to specify the nested list it should print from (still using indices format)
+# The second [2] specifies what it should print from nested list [2], all protocols duly observed
