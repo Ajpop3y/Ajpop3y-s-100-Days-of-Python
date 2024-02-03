7e48b0c26eaf5c107dcf957e1722a33fdffba769
Ajpop3y <155366498+Ajpop3y@users.noreply.github.com>
2024-02-03 01:14:03 +0100

    Create 21 Day 4 Random Treasure Map.py

---
+++ 21 Day 4 Random Treasure Map.py
@@ -0,0 +1,37 @@
+# You are going to write a program that will mark a spot on a map with an X.
+# REDUNDANT
+row1 = [" ", " ", " "]
+row2 = [" ", " ", " "]
+row3 = [" ", " ", " "]
+map = ["row1", "row2", "row3"]
+# print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure?\n")
+hori = position[0]
+vert = position[1]
+print(hori + vert)
+if position == 11:
+    row1[0] = "X"
+    print(f"{row1}\n{row2}\n{row3}")
+elif position == 12:
+    row1[1] = "X"
+    print(f"{row1}\n{row2}\n{row3}")
+elif position == 13:
+    row1[2] = "X"
+    print(f"{row1}\n{row2}\n{row3}")
+elif position == 21:
+    print("shit")
+elif position == 22:
+    print("shit")
+elif position == 23:
+    print("shit")
+elif position == 31:
+    print("shit")
+elif position == 32:
+    print("shit")
+elif position == 33:
+    print("shit")
+else:
+    print("Naa")
+
+#Horizontal column - Vertical row
+
